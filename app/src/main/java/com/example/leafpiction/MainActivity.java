1062301f5638306e3036092e8398584aeb54396e
AngelikaJustine <fay.ariana@gmail.com>
2020-06-19 17:20:08 +0700

    Fragment and Tab Color

--- app/src/main/java/com/example/leafpiction/MainActivity.java
+++ app/src/main/java/com/example/leafpiction/MainActivity.java
@@ -1,15 +1,50 @@
 package com.example.leafpiction;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.history:
+                        selectedFragment = new HistoryFragment();
+                        break;
+                    case R.id.about:
+                        selectedFragment = new AboutFragment();
+                        break;
+                    case R.id.setting:
+                        selectedFragment = new SettingFragment();
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                return true;
+            }
+        });
 
     }
 
